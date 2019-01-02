41bc9256799db16c18add7f33021c3a90ca97cf7
Ben White <benjiman.white@gmail.com>
2019-01-02 11:45:00 -0800

    added strong parameters, ran initial plant model db migration, create action saves the plant

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -4,9 +4,21 @@ class PlantsController < ApplicationController
   end
 
   def create
+    @plant = Plant.new(plant_params)
+
+    @plant.save
+
+    redirect_to @plant
   end
 
   def index
+  end
+
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :species, :age, :last_potted_date, :last_rotated_date, :notes)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_01_02_193201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plants", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.float "age"
+    t.date "last_potted_date"
+    t.date "last_rotated_date"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
