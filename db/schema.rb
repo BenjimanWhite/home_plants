fee2ef01d03d09e320bae1727337a5b4cfd3c910
Ben White <benjiman.white@gmail.com>
2019-01-03 14:20:52 -0800

    first pass at user model. Add name and email attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_193201) do
+ActiveRecord::Schema.define(version: 2019_01_03_220646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2019_01_02_193201) do
     t.date "last_potted_date"
     t.date "last_rotated_date"
     t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
