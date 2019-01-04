bf7fb696585ed6cb1fc53356b34bf79e42e262d8
Ben White <benjiman.white@gmail.com>
2019-01-04 11:33:36 -0800

    Temporarily remove data from fixture, add index on user email attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_220646) do
+ActiveRecord::Schema.define(version: 2019_01_04_192706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_01_03_220646) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
