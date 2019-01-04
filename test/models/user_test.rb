f1d4561024a815522a77e7649b897e6819fda638
Ben White <benjiman.white@gmail.com>
2019-01-03 16:06:49 -0800

    Add validations for user attributes and associated tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(name: "Thelonius Monk", email: "thelonius@yahoo.com")
+  end
+
+  test "user should be valid" do
+    assert @user.valid?
+  end
+    
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "name is not too long" do
+    @user.name = "z" * 101
+    assert_not @user.valid?
+  end
+
+  test "email is not too long" do
+    @user.email = "z" * 101 + "@foo.com"
+    assert_not @user.valid?
+  end
+
+  test "allowed email addresses are valid" do
+    valid_emails = ["coltrane@gmail.com", "COLTRANE@GMAIL.COM",
+                    "COL-TRA_ne@abc.zyx.net", "col.trane@gmail.com",
+                    "col+tRAne@training.org"]
+
+    valid_emails.each do |email|
+      @user.email = email
+
+      assert @user.valid?, "#{email.inspect} should be valid" 
+    end
+  end
+
+  test "disallowed email addresses are invalid" do
+    invalid_emails = ["coltrane_at_gmail.com", "COLTRANE@GMAIL,COM",
+                      "COL-TRA_ne@abc@zyx.net", "col.trane@gmail/com",
+                      "col+RAne@train+ing.org"]
+
+    invalid_emails.each do |email|
+      @user.email = email
+
+      assert_not @user.valid?, "#{email.inspect} should be invalid" 
+    end
+  end
+
 end
